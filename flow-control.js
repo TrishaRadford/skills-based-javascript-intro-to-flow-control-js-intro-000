53a006a2409cbf43c51e3f850adb15c7adb6e173
convivial-reducer-7231 <tradford10@gmail.com>
2020-10-05 00:43:41 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -49,9 +49,6 @@ function switchAge(age) {
     case "18" :
     console.log("You are a teenager!");
     break;
-    case "18" :
-    console.log("You are a teenager!");
-    break;
     case "19" :
     console.log("You are a teenager!");
     break;
