625ca6d674eaf0f96be3c32fe3b9f25470ffdcfc
convivial-reducer-7231 <tradford10@gmail.com>
2020-10-05 02:01:16 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,11 +27,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-      if (age ? '>=13' : '>20') {
-        return "You are a teenager!";
-    } else {
-      return "You have an age";
-    }
+      return (age >=13 && age <=19) ? "You are a teenager!" : "You are not a teenager"; 
 }
 
 function switchAge(age) {
